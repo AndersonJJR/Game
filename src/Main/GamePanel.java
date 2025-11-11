8d9a74507bbd179877dbd05c5bad4ec6c91904ab
MarcosA-Godinho <marcos.godinho13@gmail.com>
2025-11-11 19:56:16 -0300

    Alteração tamanho player

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -47,7 +47,7 @@ public class GamePanel extends JPanel {
         // Desenha o jogador
         if (player.getSprite() != null) {
             g.drawImage(player.getSprite(), player.getPlayerX(), player.getPlayerY(),
-                    128, 128, null);
+                    128, 64 , null);
         }
 
         // --- CORREÇÃO AQUI ---
