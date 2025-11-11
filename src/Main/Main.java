7445b4fb1f177faa304c0e0bcf79c604b9379e16
AndersonJJR <adasilvachavesjunior@gmail.com>
2025-11-11 20:09:19 -0300

    Teste

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,14 @@
 package Main;
 
 import javax.swing.*;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Locale setLocale = Locale.getDefault();
         // Inicia o Swing
         SwingUtilities.invokeLater(new Runnable() {
             @Override
