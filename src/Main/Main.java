a665e5a8a3bc6f32f0c5dda0ba2918e3bc68585a
AndersonJJR <adasilvachavesjunior@gmail.com>
2025-11-16 16:22:27 -0300

    Teste

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Locale setLocale = Locale.getDefault();
         // Inicia o Swing
         SwingUtilities.invokeLater(new Runnable() {
             @Override
