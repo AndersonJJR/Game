02f0c3a2af74134bc4568a84979094768f8dc9cd
Marcos Godinho <marcos.godinho13@gmail.com>
2025-11-24 19:10:14 -0300

    Add files via upload

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,13 +1,10 @@
 package Main;
 
 import javax.swing.*;
-import java.util.Locale;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
         // Inicia o Swing
         SwingUtilities.invokeLater(new Runnable() {
             @Override
